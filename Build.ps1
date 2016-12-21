fb8166e7b159b140b63f6b2b83579318f819b449
Chris Richards <chris.richards@yellowfeather.co.uk>
2016-12-21 14:39:28 +0000

    Updates Build.ps1 to osx.10.12-x64

--- Build.ps1
+++ Build.ps1
@@ -58,5 +58,5 @@ $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BU
 $revision = "{0:D}" -f [convert]::ToInt32($revision, 10)
 
 exec { & dotnet publish -c Release -r win10-x64 -o .\artifacts/win10-x64 --version-suffix=$revision }
-exec { & dotnet publish -c Release -r osx.10.10-x64 -o .\artifacts/osx.10.10-x64 --version-suffix=$revision }
+exec { & dotnet publish -c Release -r osx.10.12-x64 -o .\artifacts/osx.10.12-x64 --version-suffix=$revision }
 exec { & dotnet publish -c Release -r ubuntu.14.04-x64 -o .\artifacts/ubuntu.14.04-x64 --version-suffix=$revision }
